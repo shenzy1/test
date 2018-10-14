c7c8b6b3dde22fce5e75d987355380d1853ecfa5
sublum <sublum@126.com>
2018-10-14 19:15:49 +0800

    本地工程添加到git

---
+++ GitTest.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
